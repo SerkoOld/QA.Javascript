f85228fa3dcde413a2ff550319837217f9a58117
Darshit Pandya <darshit.pandya@serko.com>
2020-11-03 13:40:13 +1300

    Add Javascript test

---
+++ test/specs/zenoLogin.spec.js
@@ -0,0 +1,9 @@
+describe('Zeno Login Tests', () => {
+    var url = 'https://general.testing.serko.travel/login'
+    var userId = '';
+    var password = '';
+
+    it('Test 1', async () => {
+        browser.url(url)
+    })
+})
