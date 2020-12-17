c5a8dcf9e496d17e9a8f004b1b2b78a66caeec3a
Darshit Pandya <darshit.pandya@serko.com>
2020-12-17 13:17:07 +1300

    Update describe and it

--- test/specs/zenoLogin.spec.js
+++ test/specs/zenoLogin.spec.js
@@ -1,6 +1,6 @@
 import LoginPage from '../Pages/login.page'
-describe('login form', () => {
-    it('should access with right creds', () => {
+describe('write your own describe', () => {
+    it('write your own test case', () => {
        
     })
 })
