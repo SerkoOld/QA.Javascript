200d92afa90e3311e2ac0bddc5fa57f0bba33f16
Peter Yu <peter.yu@serko.com>
2021-03-24 13:08:06 +1300

    Updated test flows

--- src/option1_webdriverio/index.test.js
+++ src/option1_webdriverio/index.test.js
@@ -1,4 +1,19 @@
-it('should open login pages', () => {
-    browser.url('http://localhost:3000')
-    expect(browser.getUrl()).toContain('http://localhost:3000');
+var url = 'http://localhost:3000';
+var username = 'Katharina_Bernier';
+var password = 's3cret';
+
+it('Should be able to login with valid username and password', () => {
+    browser.url(url);
+    expect(browser.getUrl()).toContain(url);
+    $('#username').setValue(username);
+    $('#password').setValue(password);
+    $('[data-test=signin-submit]').click();
+})
+
+it('Should be able to make a new payment', () => {
+    $('[data-test="nav-top-new-transaction"]').click();
+    $('[data-test="user-list-item-qywYp6hS0U"]').click();
+    $('#amount').setValue(100);
+    $('#transaction-create-description-input').setValue('Testing');
+    $('[data-test="transaction-create-submit-payment"]').click();
 })
